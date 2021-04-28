6312a086339a2ffa656584ba72976c5c1aabb271
Lucian Cooper <cooperlucian@gmail.com>
2021-04-27 20:50:41 -0400

    refactor: make `splitChars` a generator function

--- test/splitChars.test.js
+++ test/splitChars.test.js
@@ -2,34 +2,34 @@ const { splitChars } = require('..');
 
 describe('splitChars', () => {
     test('handles zero-length strings', () => {
-        expect(splitChars('')).toStrictEqual([]);
+        expect([...splitChars('')]).toStrictEqual([]);
     });
 
     test('handles non-string inputs', () => {
-        expect(splitChars(10)).toStrictEqual([]);
+        expect([...splitChars(10)]).toStrictEqual([]);
     });
 
     test('splits plain strings', () => {
-        expect(splitChars('foo')).toStrictEqual(['f', 'o', 'o']);
+        expect([...splitChars('foo')]).toStrictEqual(['f', 'o', 'o']);
     });
 
     test('does not split between a carriage return & line feed', () => {
-        expect(splitChars('a\r\nb')).toStrictEqual(['a', '\r\n', 'b']);
+        expect([...splitChars('a\r\nb')]).toStrictEqual(['a', '\r\n', 'b']);
     });
 
     test('splits strings with combining diacritical marks', () => {
-        expect(splitChars('Ĺo͂řȩm̅')).toStrictEqual(['Ĺ', 'o͂', 'ř', 'ȩ', 'm̅']);
+        expect([...splitChars('Ĺo͂řȩm̅')]).toStrictEqual(['Ĺ', 'o͂', 'ř', 'ȩ', 'm̅']);
     });
 
     test('splits hindi strings with combining marks', () => {
-        expect(splitChars('अनुच्छेद')).toStrictEqual(['अ', 'नु', 'च्', 'छे', 'द']);
+        expect([...splitChars('अनुच्छेद')]).toStrictEqual(['अ', 'नु', 'च्', 'छे', 'द']);
     });
 
     test('splits hangul syllables', () => {
-        expect(splitChars('뎌쉐련쩨뼕')).toStrictEqual(['뎌', '쉐', '련', '쩨', '뼕']);
+        expect([...splitChars('뎌쉐련쩨뼕')]).toStrictEqual(['뎌', '쉐', '련', '쩨', '뼕']);
     });
 
     test('splits emoji sequences', () => {
-        expect(splitChars('🌷🎁💩😜👍🇺🇸🇬🇷🏳️‍🌈')).toStrictEqual(['🌷', '🎁', '💩', '😜', '👍', '🇺🇸', '🇬🇷', '🏳️‍🌈']);
+        expect([...splitChars('🌷🎁💩😜👍🇺🇸🇬🇷🏳️‍🌈')]).toStrictEqual(['🌷', '🎁', '💩', '😜', '👍', '🇺🇸', '🇬🇷', '🏳️‍🌈']);
     });
 });
